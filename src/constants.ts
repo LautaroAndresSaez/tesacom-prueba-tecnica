c2d746a4ecbda36c0836c051fc2fd4422a4ba1de
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2023-10-23 00:14:31 -0300

    feat: add ascii constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 export const FLOAT_BITS = 32;
 export const BYTE_LENGTH = 8;
+
 export const ASCII_LENGTH = 7;
+export const TEXT_END = "#0";
 
 export const FLOAT_EXP_LENGTH = 8;
 export const FLOAT_COEFF_LENGTH = 23;
