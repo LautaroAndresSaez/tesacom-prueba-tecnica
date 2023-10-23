04bb0ebebdb9078073becf1ef3fc321573896640
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2023-10-22 23:14:23 -0300

    feat: create intToBits test

--- src/parsers/bits-parsers.test.ts
+++ src/parsers/bits-parsers.test.ts
@@ -1,4 +1,4 @@
-import { floatToBits } from "./bits-parsers";
+import { floatToBits, intToBits } from "./bits-parsers";
 
 describe("Test in bits parsers functions", () => {
   it("should convert positive float to bits", () => {
@@ -9,5 +9,21 @@ describe("Test in bits parsers functions", () => {
   it("should convert negative float to bits", () => {
     const bits = floatToBits(-12.5);
     expect(bits).toBe("11000001010010000000000000000000");
+  });
+
+  it("should encode 0", () => {
+    const result = intToBits(0, 5);
+    expect(result).toBe("00000");
+  });
+
+  it("should encode negative number using 4 bits", () => {
+    const result = intToBits(-4, 4);
+    expect(result).toBe("1100");
+  });
+
+  it("should enconde positive number using 6 bits", () => {
+    const result = intToBits(4, 8);
+    expect(result).toHaveLength(8);
+    expect(result).toBe("00000100");
   });
 });
