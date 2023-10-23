7be95f5f05190c56d0de82d7e5b254d526d6cc36
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2023-10-23 09:38:42 -0300

    feat: add encode and decode test and reversibility test

---
+++ src/e2e.test.ts
@@ -0,0 +1,34 @@
+import { decode, encode } from ".";
+import { Format } from "./types/format";
+
+describe("Test encode-decode", () => {
+  it("should decode-encode without float", () => {
+    const format: Format = [
+      { tag: "PTemp", type: "int", len: 12 },
+      { tag: "BattVolt.value", type: "int", len: 12 },
+      { tag: "WaterLevel", type: "int", len: 8 },
+    ];
+    const data = { PTemp: 268, BattVolt: { value: 224 }, WaterLevel: 115 };
+    const result = encode(data, format);
+    const decodeResult = decode(result.buffer, format);
+    expect(decodeResult).toMatchObject(decodeResult);
+    expect(result.size).toBe(32);
+  });
+
+  it("should decode-encode with floats", () => {
+    const format: Format = [
+      { tag: "PTemp", type: "int", len: 12 },
+      { tag: "BattVolt.value", type: "float" },
+      { tag: "WaterLevel", type: "int", len: 8 },
+    ];
+    const data = {
+      PTemp: 268,
+      BattVolt: { value: 12.1212 },
+      WaterLevel: 115,
+    };
+    const encodeResult = encode(data, format);
+    const decodeResult = decode(encodeResult.buffer, format) as typeof data;
+    expect(encodeResult.size).toBe(52);
+    expect(decodeResult.BattVolt.value).toEqual(data.BattVolt.value);
+  });
+});
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { encode } from ".";
+import { decode, encode } from ".";
 import { Format } from "./types/format";
 
 describe("Test encode", () => {
@@ -12,5 +12,40 @@ describe("Test encode", () => {
     const result = encode(data, format);
     expect(result.buffer.toString("hex")).toBe("10c0e073");
     expect(result.size).toBe(32);
+  });
+});
+
+describe("Test decode", () => {
+  it("should decode int values", () => {
+    const buffer = Buffer.from("010203", "hex");
+    const format: Format = [
+      { tag: "v0", type: "int", len: 8 },
+      { tag: "v1", type: "int", len: 8 },
+      { tag: "v2", type: "int", len: 8 },
+    ];
+    expect(decode(buffer, format)).toMatchObject({
+      v0: 1,
+      v1: 2,
+      v2: 3,
+    });
+  });
+
+  it("should decode format with nested tags", () => {
+    const format: Format = [
+      { tag: "var0.temp", type: "uint", len: 2 },
+      { tag: "var0.temp2", type: "int", len: 3 },
+      { tag: "var1.value", type: "uint", len: 3 },
+    ];
+    const buffer = Buffer.from("bc", "hex"); // 1011-1100
+    const result = decode(buffer, format);
+    expect(result).toMatchObject({
+      var0: {
+        temp: 2,
+        temp2: -1,
+      },
+      var1: {
+        value: 4,
+      },
+    });
   });
 });
