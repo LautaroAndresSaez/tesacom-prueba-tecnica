081171dbf55767a10ca5d4c2e59f6a5306b1d605
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2023-10-22 23:09:42 -0300

    feat: create floatToBits function using IEEE 754

---
+++ src/parsers/bits-parsers.ts
@@ -0,0 +1,23 @@
+import {
+  FLOAT_COEFF_LENGTH,
+  FLOAT_EXP_LENGTH,
+  FLOAT_EXP_OFFSET,
+} from "../constants";
+import { NumberToBitsParser } from "../types/parsers";
+
+export const floatToBits: NumberToBitsParser = (value) => {
+  const sign = value < 0 ? "1" : "0";
+  //Need positive value to represent exponent and coefficient
+  value = Math.abs(value);
+  const stringValue = value.toString(2);
+  const rawExp = stringValue.split(".")[0].length - 1 + FLOAT_EXP_OFFSET;
+  const exp = rawExp.toString(2).padStart(FLOAT_EXP_LENGTH, "0");
+
+  const coeff = stringValue
+    .split("")
+    .filter((x, i) => x !== "." && i !== 0)
+    .join("")
+    .padEnd(FLOAT_COEFF_LENGTH, "0");
+
+  return `${sign}${exp}${coeff}`;
+};
